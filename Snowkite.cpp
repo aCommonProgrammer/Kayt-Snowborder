f335ada403615ed375cbecc47bb01f72b4a36d9e
aCommonProgrammer <isanigabri@gmail.com>
2023-02-24 20:43:46 +0100

    Aggiunta classe podio

--- Snowkite.cpp
+++ Snowkite.cpp
@@ -9,31 +9,34 @@
 #include <fstream>
 #include <time.h>
 
-#define CARATTERE_SEPARATORE '.'
+#define CARATTERE_SEPARATORE '-'
+#define PARTECIPANTI 40
+#define MINUTI_GARA 30
 
 using namespace std;
 
-//------------------------------------------CLASSE-GENERA-DISTANZE---------------------------------------------
-
+//------------------------------------------STRUCT-KITESNOWBOARDER---------------------------------------------
 /*!
     \struct KiteSnowboarder
     \brief contiene il nome e i punti rilevati di un atleta
 */
-
 struct KiteSnowboarder
 {
-/*!
-    \var nome
-    \brief nome dell'atleta
-*/
+/*! \var nome
+    \brief nome dell'atleta*/
     string nome;
-/*!
-    \var misurazione
-    \brief contiene i punti rilevati durante il percorso dell'atleta
-*/
-    int misurazione[30][2];
-};
 
+/*! \var minuti
+    \brief tempo in minuti della prestazione*/
+    int minuti = 0;
+
+/*! \var misurazione
+    \brief contiene i punti rilevati durante il percorso dell'atleta*/
+    int misurazione[MINUTI_GARA][2] = {{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1},{-1,-1}};
+};
+//-------------------------------------------------------------------------------------------------------------
+
+//------------------------------------------CLASSE-GENERA-DISTANZE---------------------------------------------
 /*!
     \class GeneraDistanze
     \brief simula i dati che dovrebbero essere rilevati in una gara
@@ -42,61 +45,80 @@ class GeneraDistanze
 {
     public:
     /*!
-        \var file_name
-        \brief nome del file dove � contenuto il registro gara
+        \fn getFileName
+        \brief restituisce il nome del file di registro
     */
-        string file_name = "RegistroGara.txt";
+    string getFileName()
+    {
+        return file_name;
+    }
+
+    /*!
+        \fn setFileName
+        \param[in] name nome del file
+        \brief imposta il nome del file di registro
+    */
+    void setFileName(string name)
+    {
+        file_name = name;
+    }
 
     /*!
         \fn creaRegistroGara
-        \brief genera i dati degli atleti e li passa uno ad uno alla funzione writeKiteSnowboarder(KiteSnowboarder athlete)
+        \brief genera i dati degli atleti e li passa uno ad uno alla funzione writeKiteSnowboarder()
     */
         void creaRegistroGara()
         {
-            int n = rand()%40;
+            ofstream f(file_name);
+            f.close();
+            int n = rand()%PARTECIPANTI;
             for (int c=0; c<n; c++)
             {
-                int m=rand()%30;
+                int m=rand()%MINUTI_GARA;
                 writeKiteSnowboarder(generaGiro(generaNome(), m), m);
             }
         }
 
-        /*!
+    /*!
         \fn writeKiteSnowboarder
         \param[in] athlete atleta da inserire nel registro
         \param[in] minuti tempo della prestazione in minuti
         \brief scrive nel file del registro l'ateta passato come parametro
-        */
+        \note Il file viene scritto nel formato "nome cognome-minuti-x0-y0-x1-y1-x2-y2...-"
+    */
         void writeKiteSnowboarder(KiteSnowboarder athlete, int minuti)
         {
-            ofstream fout(file_name);
-            fout<<athlete.nome<<CARATTERE_SEPARATORE;
+            fstream fout(file_name, ios::app);
+            fout<<athlete.nome<<CARATTERE_SEPARATORE<<athlete.minuti<<CARATTERE_SEPARATORE;
                 for (int t=0; t<minuti; t++)
                 {
-                    fout<<athlete.misurazione[t][0]<<CARATTERE_SEPARATORE<<athlete.misurazione[t][1];
-                    if (t != minuti-1)
-                        fout<<CARATTERE_SEPARATORE;
-                    else
+                    fout<<athlete.misurazione[t][0]<<CARATTERE_SEPARATORE<<athlete.misurazione[t][1]<<CARATTERE_SEPARATORE;
+                    if (t == minuti-1)
                         fout<<'\n';
                 }
-                fout.close();
+            fout.close();
         }
 
     private:
-        /*!
+    /*! \var file_name
+        \brief nome del file dove è contenuto il registro gara*/
+        string file_name = "RegistroGara.txt";
+
+    /*!
         \fn generaGiro
         \param[in] nome nome dell'atleta
-        \param[in] n_minuti tempo effettuato dall'atleta (max 30)
+        \param[in] n_minuti tempo effettuato dall'atleta
         \brief genera la prestazione di un atleta
         \return atleta con prestazione genrata
-        */
+    */
         KiteSnowboarder generaGiro(string nome, int n_minuti)
         {
-            if(n_minuti > 30)
-                n_minuti = 30;
+            if(n_minuti > MINUTI_GARA)
+                n_minuti = MINUTI_GARA;
 
             KiteSnowboarder atleta;
             atleta.nome = nome;
+            atleta.minuti = n_minuti;
             for(int m=0; m<n_minuti; m++)
             {
                 atleta.misurazione[m][0] = rand()%101;
@@ -117,11 +139,11 @@ class GeneraDistanze
             string nome, cognome;
             for (int c=0, r=rand()%9224; c<r; c++)
             {
-                fin_nome>>nome;
+                getline(fin_nome, nome);
             }
             for (int c=0, r=rand()%872; c<r; c++)
             {
-                fin_cognome>>cognome;
+                getline(fin_cognome, cognome);
             }
             fin_nome.close(); fin_cognome.close();
             return nome+" "+cognome;
@@ -129,11 +151,89 @@ class GeneraDistanze
 };
 //-------------------------------------------------------------------------------------------------------------
 
+//--------------------------------------CLASSE-CALCOLA-PODIO---------------------------------------------------
+/*!
+    \class CalcolaPodio
+    \brief calcola il podio dei primi 3 atleti
+*/
+class CalcolaPodio
+{
+    public:
+    /*!
+        \fn getFileName
+        \brief restituisce il nome del file di registro
+    */
+    string getFileName()
+    {
+        return file_name;
+    }
+
+    /*!
+        \fn setFileName
+        \param[in] name nome del file
+        \brief imposta il nome del file di registro
+    */
+    void setFileName(string name)
+    {
+        file_name = name;
+    }
+
+    /*!
+        \fn leggiRegistro
+        \brief legge il file di registro e lo trascrive in un vettore
+        \return vettore contenente i dati del file
+    */
+    KiteSnowboarder* leggiRegistro()
+    {
+        KiteSnowboarder vettore[PARTECIPANTI];
+        ifstream fin(file_name);
+        string app_interi;
+        int contatore = 0;
+        while(fin)
+        {
+            getline(fin, vettore[contatore].nome, CARATTERE_SEPARATORE);
+            getline(fin, app_interi, CARATTERE_SEPARATORE);
+            vettore[contatore].minuti = stoi(app_interi);
+            for (int m=0; m<vettore[contatore].minuti; m++)
+            {
+                getline(fin, app_interi, CARATTERE_SEPARATORE);
+                vettore[contatore].misurazione[0][m] = stoi(app_interi);
+                getline(fin, app_interi, CARATTERE_SEPARATORE);
+                vettore[contatore].misurazione[1][m] = stoi(app_interi);
+            }
+            contatore++;
+        }
+        fin.close();
+        return vettore;
+    }
+
+    /*!
+        \fn podio
+        \brief calcola il podio dei vincitori
+        \return vettore contenente il podio
+    */
+    KiteSnowboarder* podio()
+    {
+
+    }
+
+ private:
+    /*! \var file_name
+        \brief nome del file dove è contenuto il registro gara*/
+        string file_name = "RegistroGara.txt";
+
+
+};
+//-------------------------------------------------------------------------------------------------------------
 
 //---------------------------------------------MAIN------------------------------------------------------------
 int main()
 {
     srand(time(NULL));
+    GeneraDistanze Generatore;
+    Generatore.creaRegistroGara();
+    CalcolaPodio Calcolatore;
+    KiteSnowboarder* partecipanti = Calcolatore.leggiRegistro();
 
 
     return 0;
